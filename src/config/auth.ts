0e927dc9a52fd4f11709fec98637d9ee25f09739
Fernando Freire <fernando.ricardo.freire@gmail.com>
2022-01-16 17:35:16 -0300

    rotas autenticadas

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '90eb2d7684ff4faa1b2d29c968114dd7fe2ba9df',
+    expiresIn: '7d',
+  },
+};
--- src/services/CreateSessionService.ts
+++ src/services/CreateSessionService.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
+import authConfig from '../config/auth';
 import User from '../models/User';
 
 interface Request {
@@ -32,9 +33,11 @@ class CreateSessionSerivce {
       throw new Error('Incorrect email/password combination.');
     }
 
-    const token = sign({}, '90eb2d7684ff4faa1b2d29c968114dd7fe2ba9df', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '7d',
+      expiresIn,
     });
 
     return {
